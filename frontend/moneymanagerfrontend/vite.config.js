c62d65f82a227ecaba5206b5cef2d352fe27a5b9
Chinmay Chavan <maxchinmay555@gmail.com>
2026-02-17 23:35:41 +0530

    vite config changes

--- frontend/moneymanagerfrontend/vite.config.js
+++ frontend/moneymanagerfrontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: '/', // Add this to ensure assets load from the correct root path
 })
